f9251b7fbdaf91ae917fb2354805388e8ddb63a4
hayapy88 <payagram01@gmail.com>
2024-10-20 23:59:45 +1100

    Create the inquiry creation API

--- server/routes/inquiries.js
+++ server/routes/inquiries.js
@@ -1,8 +1,25 @@
 const express = require("express");
+const Inquiry = require("../models/Inquiry");
 const router = express.Router();
 
 // Post /inquiry
 router.post("/", (req, res) => {
+  try {
+    const { name, email, message } = req.body;
+    const inquiry = Inquiry.create({ name, email, message });
+    res.status(201).json({
+      status: "success",
+      message: "Your inquiry was created successfully",
+      inquiry: inquiry,
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "failed",
+      message: "Failed to create inquiry",
+      error: error.massage,
+    });
+  }
+
   console.log(req.body);
   res.send("Inquiry created");
 });
