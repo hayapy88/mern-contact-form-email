dfee4d0f10e163024c3246da2943461c22941ed2
hayapy88 <payagram01@gmail.com>
2024-10-24 01:46:47 +1100

    Install React, Tailwind CSS, and axios

---
+++ client/src/App.jsx
@@ -0,0 +1,8 @@
+import "./App.css";
+const React = require("react");
+
+function App() {
+  return <div className="App"></div>;
+}
+
+export default App;
---
+++ client/tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  theme: {
+    screens: {
+      xs: "375px",
+      sm: "480px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1440px",
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "6rem",
+      },
+    },
+    extend: {},
+  },
+  plugins: [],
+};
