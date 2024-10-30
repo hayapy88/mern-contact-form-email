155b97e456b5ea03e40884236b5e499c9ee0758c
hayapy88 <payagram01@gmail.com>
2024-10-31 01:18:08 +1100

    Add a debug code for validation

--- server/routes/inquiries.js
+++ server/routes/inquiries.js
@@ -22,6 +22,7 @@ router.post(
   (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
+      console.log("Validation errors:", errors.array());
       return res.status(400).json({
         status: "failed",
         errors: errors.array(),
