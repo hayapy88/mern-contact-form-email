7fef16ba51eaf826023c8210648109fca6a9309e
hayapy88 <payagram01@gmail.com>
2024-11-03 23:57:34 +1100

    Add a functionality to send to admin email address as well

--- server/controllers/sendEmail.js
+++ server/controllers/sendEmail.js
@@ -9,18 +9,28 @@ const sendEmail = async (to, subject, html) => {
     },
   });
 
-  const mailOptions = {
+  const mailToCustomer = {
     from: process.env.GMAIL_EMAIL,
     to: to,
     subject: subject,
     html: html,
   };
+  const mailToAdmin = {
+    from: process.env.GMAIL_EMAIL,
+    to: process.env.ADMIN_EMAIL,
+    subject: subject,
+    html: html,
+  };
 
   try {
-    await transporter.sendMail(mailOptions);
-    console.log("Email sent successfully");
+    await transporter.sendMail(mailToCustomer);
+    console.log("Email sent to Customer successfully");
+    await transporter.sendMail(mailToAdmin);
+    console.log("Email sent to Admin successfully");
+    res.status(200).json({ message: "Email sent successfully" });
   } catch (error) {
     console.error("Failed to send email:", error);
+    res.status(500).json({ error: "Failed to send email" });
     throw new Error("Failed to send email");
   }
 };
